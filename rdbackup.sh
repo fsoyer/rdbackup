d772901d2f2ffc8efd295c5566f7386004395881
fgth <fsoyer@systea.fr>
2018-09-21 16:39:44 +0200

    format README

--- rdbackup.sh
+++ rdbackup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 # Backup Script (RDIFF-BACKUP over SSH and RSYNC)
-# (c) Frank Soyer <frank.soyer@gmail.com> 2010
-#
 # v3.5
 # See README.md for details
+# (c) 2005, Frank Soyer <frank.soyer@gmail.com>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# The GNU General Public License is available at:
+# http://www.gnu.org/copyleft/gpl.html
 
 ERROR_FLAG=0
 ERROR=0
